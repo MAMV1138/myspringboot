3cbb14bc7b6037f905f837e2ca3edb3e7014a4c5
mamv1138 <mamv1138@gmail.com>
2023-01-22 02:13:15 -0300

    progress mvc: updating form data

--- apps/grade-submission/src/main/java/com/ltp/gradesubmission/GradesController.java
+++ apps/grade-submission/src/main/java/com/ltp/gradesubmission/GradesController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GradesController {
@@ -15,20 +16,46 @@ public class GradesController {
 
     @GetMapping("/grades")
     public String index(Model model) {
+
         model.addAttribute("grades", studentGrades);
         return "grades";
     }
 
     @GetMapping("/form")
-    public String myForm(Model model) {
-        model.addAttribute("grade", new Grade());
+    public String myForm(Model model, @RequestParam(required = false) String name) {
+        int result = findIndex(name);
+
+        if (result == -10) {
+            model.addAttribute("grade", new Grade());
+        } else {
+            model.addAttribute("grade", studentGrades.get(result));
+        }
+
         return "form";
     }
 
     @PostMapping("/submitHandler")
     public String submitForm(Grade grade) {
-        studentGrades.add(grade);
+        // if grade is not in the studentslist then insert
+        int index = findIndex(grade.getName());
+        if (index == -10) {
+            studentGrades.add(grade);
+        } else {
+            studentGrades.set(index, grade);
+        }
+        // if grade is in the studentlist then modify object
+
         return "redirect:/grades";
+    }
+
+    private int findIndex(String name) {
+
+        for (int i = 0; i < studentGrades.size(); i++) {
+            if (studentGrades.get(i).getName().equals(name)) {
+                return i;
+            }
+        }
+        return -10;
     }
 
 }
